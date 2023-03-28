8c854200400637d87b0bfa89eee8356832965650
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-03-28 10:06:13 -0300

    New Entity Enrollment

---
+++ src/main/java/com/myapi/example/model/Enrollment.java
@@ -0,0 +1,32 @@
+package com.myapi.example.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = Enrollment.TABLE_NAME)
+public class Enrollment {
+    public static final String TABLE_NAME = "enrollment";
+
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false, updatable = false)
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false, updatable = false)
+    private Course course;
+
+    @Column(name = "date_creation")
+    @NotNull
+    private Date date_creation;
+
+}
